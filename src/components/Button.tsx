faa437bcb6080f0118ebc0e6c4c1f936e2267ea9
Michael Vurchio <michael@micantoine.dev>
2020-09-19 00:11:09 +0900

    Pass property `classname` to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface ButtonProps {
+  className?: string;
   children: React.ReactNode;
   variant?: string;
   onClick: () => void;
@@ -9,6 +10,7 @@ interface ButtonProps {
 function Button(props:ButtonProps) {
   return (
     <button
+      className={props.className}
       onClick={props.onClick}
       data-oo-button={props.variant ? props.variant : ''}>
       {props.children}
